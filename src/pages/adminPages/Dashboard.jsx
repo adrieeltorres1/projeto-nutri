4f490af1249593e8e33fd916f7fc460d78b0a944
adrieeltorres1 <adrieltorreslds@gmail.com>
2025-02-26 20:18:54 -0300

    Update

--- src/pages/adminPages/Dashboard.jsx
+++ src/pages/adminPages/Dashboard.jsx
@@ -3,14 +3,7 @@ import { Button } from "antd";
 const Dashboard = () => {
     return (
         <>
-            <h1 className="flex justify-between items-center">
-                Dashboard 
-                <Button 
-                className="" 
-                type="primary">
-                Novo Usuário
-                </Button>
-            </h1>
+            Dashboard 
         </>
     );
 }
